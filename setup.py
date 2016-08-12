d210aa80fcc8de1bee9e6a56b75ae8f169dae6b0
David Ko <david@barobo.com>
2016-08-12 14:22:12 -0700

    Updated pylinkbot3 requirements

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
     entry_points = {
         "console_scripts": ['linkbot-serialid-tool=linkbot_serialid_tool.linkbot_serialid_tool:main']
     },
-    install_requires = ["PyLinkbot >= 2.3.3"],
+    install_requires = ["PyLinkbot3>= 3.0.0"],
     description = "Tool for programming Linkbot serial IDs",
     zip_safe = False,
     include_package_data = True,
